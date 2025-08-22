552c97d6a80453c29463af1d6116e01c23a37f67
Jorge Cruz <u23216323@utp.edu.pe>
2025-08-21 21:01:41 -0500

    Arreglos de estilos y finalizacion de la page inicio

---
+++ src/app/componente/boton-scroll/boton-scroll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-scroll',
+  standalone: true,
+  imports: [],
+  templateUrl: './boton-scroll.component.html',
+  styleUrl: './boton-scroll.component.css'
+})
+export class BotonScrollComponent {
+
+}
